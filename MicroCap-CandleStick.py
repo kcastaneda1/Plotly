1fbfc5c3735e9e90c14b4cb168422b8b62a89e38
kcastaneda1 <castanedaken89@gmail.com>
2024-06-12 20:41:40 -0700

    Update MicroCap-CandleStick.py

--- MicroCap-CandleStick.py
+++ MicroCap-CandleStick.py
@@ -6,7 +6,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import numpy as np
 import pandas as pd
 import datetime
 import plotly.express as px
